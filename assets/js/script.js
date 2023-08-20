87378e343bf610665288bbc463c5b3008b5a31db
foldaniel85 <foldaniel@outlook.cm>
2023-08-20 12:23:04 -0300

    Finalizado valores a converte para moeda

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
 const convertButton = document.querySelector ('.currency-button')
+const currencySelect = document.querySelector('.currency-select')
 
 function convertValue() {
     const inputValue = document.querySelector('.currency-input').value
+    const currencyValueConverted = document.querySelector('.currency-value-to-converted')
     const currencyValue = document.querySelector('.currency-value')
 
     const dolarToday = 5.2
-    const convertedValue = inputValue / dolarToday
+    const euroTody = 6.2
 
-    currencyValue.innerHTML = new Intl.NumberFormat('pt-BR', {
+    if(currencySelect.value == 'dolar') {
+        currencyValue.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency:'USD'
+        }).format(inputValue / dolarToday)
+    }
+
+    if(currencySelect.value == 'euro') {
+        currencyValue.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency:'EUR'
+        }).format(inputValue / euroTody)
+    }
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL'
     }).format(inputValue)
@@ -15,6 +31,23 @@ function convertValue() {
     console.log(inputValue)
 }
 
+function changeCurrency() {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImage = document.querySelector('.currency-img')
+
+    if(currencySelect.value == 'dolar') {
+        currencyName.innerHTML = 'Dólar amaricano'
+        currencyImage.src = './assets/img/dolar.png'
+    }
+
+    if(currencySelect.value == 'euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImage.src = './assets/img/euro.png'
+    }
+
+    convertValue()
+}
+
 
 convertButton.addEventListener('click', convertValue)
-
+currencySelect.addEventListener('change', changeCurrency)
